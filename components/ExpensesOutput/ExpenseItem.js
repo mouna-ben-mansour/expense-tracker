24b415a6a88251a6011b65a0fd17014097739831
mouna-ben-mansour <benmansour.mouna2@gmail.com>
2023-10-20 15:19:58 +0100

    formatting date

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,5 +1,6 @@
 import {View, StyleSheet, Text, Pressable} from "react-native";
 import {GlobalStyles} from "../../constants/styles";
+import {getFormattedDate} from "../../util/date";
 
 function ExpenseItem({title, date, amount}) {
     return (
@@ -7,10 +8,10 @@ function ExpenseItem({title, date, amount}) {
             <View style={styles.expenseItem}>
                 <View>
                     <Text style={[styles.textBase, styles.title]}>{title}</Text>
-                    <Text style={styles.textBase}>{date.toString()}</Text>
+                    <Text style={styles.textBase}>{ getFormattedDate(date)}</Text>
                 </View>
                 <View style={styles.amountContainer}>
-                    <Text style={styles.amount}>{amount}</Text>
+                    <Text style={styles.amount}>{amount.toFixed(2)}</Text>
                 </View>
             </View>
         </Pressable>
@@ -47,7 +48,8 @@ const styles = StyleSheet.create( {
         backgroundColor: 'white',
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 4
+        borderRadius: 4,
+        minWidth:80
     },
     amount:{
         color: GlobalStyles.colors.primary500,
