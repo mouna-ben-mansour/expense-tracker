10ff3e9d0089005029fa884f119e7d7a0c930003
mouna-ben-mansour <benmansour.mouna2@gmail.com>
2023-10-21 21:49:11 +0100

    delete button

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -1,5 +1,7 @@
 import {StyleSheet,View,Text} from "react-native";
 import {useLayoutEffect} from "react";
+import IconButton from "../components/UI/IconButton";
+import {GlobalStyles} from "../constants/styles";
 
 function ManageExpenseScreen({route, navigation}) {
     const expenseId = route.params?.expenseId;
@@ -11,26 +13,38 @@ function ManageExpenseScreen({route, navigation}) {
         })
     },[navigation, isEditing])
 
-
-    if(isEditing) {
-        return(
-            <View style={styles.container}>
-                <Text>editing : {expenseId}</Text>
-            </View>
-        )
-    } else {
-        return (
-            <View style={styles.container}>
-                <Text>add</Text>
-            </View>
-        )
+    function deleteExpenseHandler() {
+        console.log('delete')
     }
+
+    return(
+        <View style={styles.container}>
+            <Text>editing : {expenseId}</Text>
+            { isEditing &&
+                <View style={styles.deleteContainer}>
+                    <IconButton icon='trash'
+                                color={GlobalStyles.colors.error50}
+                                size={36}
+                                onPress={deleteExpenseHandler}/>
+                </View>
+            }
+        </View>
+    )
 
 }
 export default ManageExpenseScreen;
 
 const styles = StyleSheet.create({
     container: {
-        flex:1
+        flex:1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary700
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center'
     }
 })
